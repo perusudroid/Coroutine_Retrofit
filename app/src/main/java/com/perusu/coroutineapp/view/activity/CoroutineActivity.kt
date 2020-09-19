307b1f5c4862e531eeaa4edf797b24385fff70df
Periya <periyamydell@gmail.com>
2020-09-19 08:43:21 -0700

    Commit from ST

--- app/src/main/java/com/perusu/coroutineapp/view/activity/CoroutineActivity.kt
+++ app/src/main/java/com/perusu/coroutineapp/view/activity/CoroutineActivity.kt
@@ -9,5 +9,7 @@ class CoroutineActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //add navigation
     }
 }
