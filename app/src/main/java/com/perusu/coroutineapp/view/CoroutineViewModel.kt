a4a9abb8bc347bdb2f4c8468f3efdc87fcdec4e5
Periya <periyamydell@gmail.com>
2020-09-19 08:30:10 -0700

    test commit

--- app/src/main/java/com/perusu/coroutineapp/view/CoroutineViewModel.kt
+++ app/src/main/java/com/perusu/coroutineapp/view/CoroutineViewModel.kt
@@ -25,6 +25,7 @@ open class CoroutineViewModel(
     val obRandomTwoDogs = liveData {
         emit(GeneralResult.Progress(true))
         emitSource(getRandomTwoDogs())
+        //test this
     }
 
     private fun getRandomTwoDogs(): LiveData<GeneralResult> = liveData {
